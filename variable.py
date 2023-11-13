ed921a9cdd866c8fbfe284dfd3120403cc39c738
KrAkashdeep <akashdeep93140@gmail.com>
2023-11-13 22:29:07 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,35 @@
+x=4
+y='akash'
+# y="akash" in double quotes also
+
+print(x)
+print(y)
+
+z=6
+z="akash"
+print(z)  #the last declare value will print if two variables are same 
+
+
+#casting
+x=str(5)
+y=int(5)
+z=float(5)
+
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+
+#variable start with letter and underscore but not with numbers
+# variables are case sensitive
+
+
+#we can declare like this 
+
+a,b,c="apple","mangoes","banana"
+print(a)
+print(b)
+print(c)
+
