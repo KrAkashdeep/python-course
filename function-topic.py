a41bec1fa30f0f47cdd3b7eabb8ee3c92859a4d6
KrAkashdeep <akashdeep93140@gmail.com>
2023-11-14 17:43:11 +0530

    functions

---
+++ function-topic.py
@@ -0,0 +1,18 @@
+# --->> to define function we use def keywords before writing function name 
+
+# -->> created function 
+
+def trailfunction():
+    print("hello world")
+
+
+# -->> calling the function like this 
+
+trailfunction()
+
+# --->> passing argument to function 
+
+def trailfunction(x):
+    print(x+" world")
+
+trailfunction("hello")
