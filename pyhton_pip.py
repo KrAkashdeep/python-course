cf76530176b5cb886a1d11c3ed503cfc023a8f7c
KrAkashdeep <akashdeep93140@gmail.com>
2023-11-15 12:16:12 +0530

    python pip

---
+++ pyhton_pip.py
@@ -0,0 +1,28 @@
+#----------------------------------python pip ----------------------------------------
+
+#pip is a package manager for python package
+
+#package
+#NumPy
+# Pendulum 
+#Python Imaging Library 
+#MoviePy 
+#Requests 
+#Tkinter 
+#PyQt 
+#Pandas 
+#Py32 
+#PyTest
+
+
+
+#check version of pip  by (py -m pip --version)
+
+#install package (py -m pip install packagename)
+
+#to uninstall package (py -m pip uninstall packagename)
+
+#to check package list (py -m pip list)
+
+#STUDY BY OWN IN MORE DETAILS
+  
